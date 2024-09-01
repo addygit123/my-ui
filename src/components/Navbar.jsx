3ea444c352e32715ab8dc845ac0ef2317b934ba0
addygit123 <minecraftaddy123@gmail.com>
2024-09-01 06:26:54 +0530

    till theme purple

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,16 @@ export default function Navbar() {
     return (
       <>
         <nav>
-            <ul className="h-12 w-screen bg-black text-slate flex gap-5 items-center shadow-lg">
+            <ul className="h-20 w-screen bg-purple-999 text-slate flex gap-5 items-center text-md font-semibold shadow-lg">
                 <img className="px-2 py-2" src="src/assets/icon.svg" alt="" />
-                <li className="text-gray-300">PROJECTS</li>
-                <li className="text-gray-300">ABOUT</li>
-                <li className="text-gray-300">GET INVOLVED</li>
-                <li className="text-gray-300">TALK</li>
-                <li className="text-gray-300">BUILD A PROJECT</li>
-                <li className="text-gray-300">NEWS</li>
-                <li className="text-gray-300 absolute right-40">SIGN IN</li>
-                <li className="text-gray-300 absolute right-8">REGISTER</li>
+                <li className="cursor-pointer text-gray-300">PROJECTS</li>
+                <li className="cursor-pointer text-gray-300">ABOUT</li>
+                <li className="cursor-pointer text-gray-300">GET INVOLVED</li>
+                <li className="cursor-pointer text-gray-300">TALK</li>
+                <li className="cursor-pointer text-gray-300">BUILD A PROJECT</li>
+                <li className="cursor-pointer text-gray-300">NEWS</li>
+                <li className="cursor-pointer text-gray-300 absolute right-40">SIGN IN</li>
+                <li className="cursor-pointer text-gray-300 absolute right-8">REGISTER</li>
                 
             </ul>
         </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,12 @@ export default {
         teal: {
           500: '#00979d',
         },
+        purple:{
+          999: "#0a0227"
+        },
+        sky:{
+          899:"#00d6fb"
+        }
       },
   },
   plugins: [],
